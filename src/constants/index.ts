eded782df68d9afd4686106897775d08e1b5b77a
hady-achkar <hady.achkar@hotmail.com>
2022-04-24 02:28:26 +0300

    images constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -19,3 +19,22 @@ export const countries = [
 ]
 
 export const socials = ['facebook', 'instagram', 'twitter', 'linkedin']
+
+export const images = [
+	{src: require('../assets/images/arla.png'), name: 'arla'},
+	{src: require('../assets/images/bolcom.png'), name: 'bolcom'},
+	{src: require('../assets/images/chocomel.png'), name: 'chocomel'},
+	{src: require('../assets/images/florensis.png'), name: 'florensis'},
+	{src: require('../assets/images/gemeentemuseum.png'), name: 'gemeentemuseum'},
+	{src: require('../assets/images/jbl.png'), name: 'jbl'},
+	{src: require('../assets/images/kempen.png'), name: 'kempen'},
+	{
+		src: require('../assets/images/koninklijke-bibliotheek.png'),
+		name: 'koninklijke-bibliotheek',
+	},
+	{src: require('../assets/images/liberty-global.png'), name: 'liberty-global'},
+	{src: require('../assets/images/lightning.png'), name: 'lightning'},
+	{src: require('../assets/images/philips.png'), name: 'philips'},
+	{src: require('../assets/images/tui.png'), name: 'tui'},
+	{src: require('../assets/images/zalando.png'), name: 'zalando'},
+]
